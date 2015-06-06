a9c8b9ea8cd59b35d24b4a833ab0c03c0d4130f7
kagakububutyou <shinnnosuke1232@yahoo.co.jp>
2015-06-06 16:01:44 +0900

    だん

---
+++ Breakout/Assets/Scriput/CSVLoader.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.IO;
+using System.Text;
+using System;
+
+public class CSVLoader : MonoBehaviour {
+
+    [SerializeField]
+    private GameObject src = null;
+
+    private void Start()
+    {
+        for (int x = 0; x < 10; x++)
+        {
+            float posX = src.transform.position.x + src.transform.localScale.x * x;
+            float posY = src.transform.position.y;
+            float posZ = src.transform.position.z;
+            Instantiate(src, new Vector3(posX, posY, posZ), Quaternion.identity);
+        }
+    }
+    
+}
---
+++ Breakout/Assets/Scriput/CollisionScript.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CollisionScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    private void OnCollisionExit(Collision collision)
+    {
+        if (collision.gameObject.name == "Block(Clone)")
+        {
+            Destroy(collision.gameObject);
+        }
+    }
+}
